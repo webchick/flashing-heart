cdfa7f39947af6fcda0617c7a796f2700e17d304
Angie Byron <332535+webchick@users.noreply.github.com>
2024-12-27 13:34:18 -0800

    Back to just a flashing heart

--- main.ts
+++ main.ts
@@ -6,35 +6,11 @@ basic.forever(function () {
         . # # # .
         . . # . .
         `)
-    music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
-    basic.showNumber(679758567586578)
-    basic.showString("Hello!")
     basic.showLeds(`
-        # # . . .
-        # # . . .
-        # # . . .
-        # # # # #
-        # # # # #
-        `)
-    basic.showLeds(`
-        . . # . .
         . . . . .
-        . . # . .
-        . . # . .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . # . # .
         . . . . .
-        # . . . #
-        . # # # .
         . . . . .
-        `)
-    basic.showLeds(`
-        # # . # #
-        . # # # .
-        # . # . #
-        . # . # .
-        . . # . .
+        . . . . .
+        . . . . .
         `)
 })
